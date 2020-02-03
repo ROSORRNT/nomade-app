dd7743a315ae5e7fc573afcf752da26a20cdca2b
Romain Sorrenti <romainsorrnti@gmail.com>
2020-02-03 16:28:23 +0100

    Rendering User Places & Using Dynamic Route Segments

---
+++ src/places/pages/UserPlaces.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+import PlaceList from "../components/PlaceList";
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Musée du Louvre",
+    description:
+      "Le Louvre possède une longue histoire de conservation artistique et historique, depuis l'Ancien Régime jusqu'à nos jours.",
+    imageUrl:
+      "https://vivreparis.fr/wp-content/uploads/2019/01/Capture-d%E2%80%99e%CC%81cran-2019-01-03-a%CC%80-14.09.52-1160x701.png",
+    address: "Rue de Rivoli, 75001 Paris",
+    location: {
+      lat: 48.860611,
+      lng: 2.33764
+    },
+    creator: "u1"
+  },
+  {
+    id: "p2",
+    title: "Musée du Louvre",
+    description:
+      "Le Louvre possède une longue histoire de conservation artistique et historique, depuis l'Ancien Régime jusqu'à nos jours.",
+    imageUrl:
+      "https://www.sortiraparis.com/images/80/18029/255462-les-jeunes-ont-la-parole-la-nocturne-au-musee-du-louvre.jpg",
+    address: "Rue de Rivoli, 75001 Paris",
+    location: {
+      lat: 48.860611,
+      lng: 2.33764
+    },
+    creator: "u2"
+  }
+];
+
+const UserPlaces = () => {
+  return <PlaceList items={DUMMY_PLACES} />;
+};
+
+export default UserPlaces;
