d5d2f60bb0f9790d460e5eb887e0711f3663ad29
Romain Sorrenti <romainsorrnti@gmail.com>
2020-02-05 15:23:34 +0100

    Creating a custom form hook (useForm)

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -1,19 +1,38 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { DUMMY_PLACES } from './UserPlaces';
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
+import { useForm } from '../../shared/hooks/form-kook';
 import './PlaceForm.css';
 import {
   VALIDATOR_REQUIRE,
   VALIDATOR_MINLENGTH
 } from '../../shared/util/validators';
 
-import { DUMMY_PLACES } from './UserPlaces';
-
 const UpdatePlace = () => {
   const placeId = useParams().placeId;
 
   const identifiedPlace = DUMMY_PLACES.find(p => p.id === placeId);
+
+  const [formState, inputHandler] = useForm(
+    {
+      title: {
+        value: identifiedPlace.title,
+        isValid: true
+      },
+      description: {
+        value: identifiedPlace.description,
+        isValid: true
+      }
+    },
+    true
+  );
+
+  const placeUpdateSubmitHandler = event => {
+    event.preventDefault();
+    console.log(formState.inputs);
+  };
 
   if (!identifiedPlace) {
     return (
@@ -24,16 +43,16 @@ const UpdatePlace = () => {
   }
 
   return (
-    <form className="place-form">
+    <form className="place-form" onSubmit={placeUpdateSubmitHandler}>
       <Input
         id="title"
         element="input"
         label="Titre"
         validators={[VALIDATOR_REQUIRE()]}
         error="S'il vous plaît, entrez un texte valide"
-        onInput={() => {}}
-        value={identifiedPlace.title}
-        valid={true}
+        onInput={inputHandler}
+        initialValue={formState.inputs.title.value}
+        initialValid={formState.inputs.title.isValid}
       />
       <Input
         id="description"
@@ -41,11 +60,11 @@ const UpdatePlace = () => {
         label="Description"
         validators={[VALIDATOR_MINLENGTH()]}
         error="S'il vous plaît, entrez une description valide"
-        onInput={() => {}}
-        value={identifiedPlace.description}
-        valid={true}
+        onInput={inputHandler}
+        initialValue={formState.inputs.description.value}
+        initialValid={formState.inputs.description.isValid}
       />
-      <Button type="submit" disabled={true}>
+      <Button type="submit" disabled={!formState.isValid}>
         Modifier le lieu
       </Button>
     </form>
